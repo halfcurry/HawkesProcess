985221669908a031d9092eae64787908cdc07744
indianauthority97 <aditya15007@iiitd.ac.in>
2018-07-25 13:30:28 +0530

    Added the thinning algo part

--- hawkes_process.py
+++ hawkes_process.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import randint
 
 class hawkes_process:
 
@@ -16,14 +17,74 @@ class hawkes_process:
         p = []
         for t_i in points:
             if t_i < t:
-                p.append(self.exponential_kernel(w,t-t_i))
+                # p.append(self.exponential_kernel(w,t-t_i))
+                p.append(k(w, t - t_i))
         return mu + sum(p)
 
-    def generate_point(self):
-        # generates a point according to hawkes intensity
-        pass
+    def generate_point(self, mu, w, alpha, k, t, points):
+        # generates a point according to hawkes intensity, i.e does simulation of a single point by thinning
+
+        # intensity value at current t : set upper bound of poisson intensity
+        lambd = self.hawkes_intensity(mu, w, alpha, k, t, points)
+
+        # generate time lag from homogeneous exp distribution with this intensity
+        s = np.random.exponential(scale = 1/lambd)
+
+        #intensity with new t = t+s
+        lambd_new = self.hawkes_intensity(mu, w, alpha, k, t + s, points)
+
+        ratio = lambd_new/lambd
+
+        # randomly accept/ reject based on if the ratio is lesser than a uniform random number
+        if ratio >= np.random.uniform():
+            #update the current time
+            t = t + s
+
+        return (t,lambd)
 
 
-    def simulate_hawkes(self):
+    def simulate_hawkes_window(self, mu, w, alpha, k, time_window, n_users):
         # thinning algorithm
-        pass
+        # suppose we need to simulate any number of events in (0-time_window) for n_users
+        t = 0
+        user_points = {}
+        for user in range(0, n_users):
+            print(user)
+            user_points[user] = []
+        all_samples = []
+        prev_t = t
+        while t < time_window:
+            prev_t = t
+            user = randint(0, n_users)
+            (t,lambd) = self.generate_point(mu, w, alpha, k, t, user_points[user])
+            if prev_t != t:
+                user_points[user].append(t)
+                all_samples.append(t)
+
+        print(user_points)
+        print(all_samples)
+
+    def simulate_hawkes_events(self, mu, w, alpha, k, N, n_users):
+        #suppose we need to simulate N events in total distributed among n_users
+        t = 0
+        user_points = {}
+        for user in range(0, n_users):
+            print(user)
+            user_points[user] = []
+        all_samples = []
+        prev_t = t
+        i = len(all_samples)
+        while i <= N:
+            prev_t = t
+            user = randint(0, n_users)
+            (t, lambd) = self.generate_point(mu, w, alpha, k, t, user_points[user])
+            if prev_t != t:
+                user_points[user].append(t)
+                all_samples.append(t)
+            i = len(all_samples)
+
+        print(user_points)
+        print(all_samples)
+
+
+
