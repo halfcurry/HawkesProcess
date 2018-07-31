b80fe596692adb780db4b1dd0d20c2d44f5ee2d6
indianauthority97 <aditya15007@iiitd.ac.in>
2018-07-31 19:08:14 +0530

    Fixed divide by zero and log nan error

--- hawkes_process_classifier.py
+++ hawkes_process_classifier.py
@@ -8,14 +8,16 @@ class hawkes_process_classifier:
         # print time_stamps
         # time_stamps=time_stamps[1]
 
-        mu = param
+        mu = np.exp(param)
+        print("Mu: ", mu)
+
         alpha = triplet[0]
         w = triplet[1]
         time_stamps = triplet[2]
 
         time_diff = time_stamps[-1] - time_stamps
         time_exp = np.exp(-w * time_diff) - 1
-        first_sum = alpha * 1.0 / w * np.sum(time_exp)
+        first_sum = (alpha * 1.0 / max(1e-10, w) )* np.sum(time_exp)
 
         # print time_stamps
 
@@ -30,10 +32,21 @@ class hawkes_process_classifier:
 
         ll = -(-mu * time_stamps[-1] + first_sum + second_sum)
 
+        print("Negative likelihood: ", ll)
+
         return ll
 
+    # def wrapper_gradient_mu(self, param, *triplet):
+    #
+    #     mu = param
+    #
+    #     del_mu = self.gradient_mu(mu, *triplet)
+    #     return np.exp(del_mu)
+
+
     def gradient_mu(self, param, *triplet):
-        mu = param
+        mu = np.exp(param)
+
         alpha = triplet[0]
         w = triplet[1]
         time_stamps = triplet[2]
@@ -45,15 +58,15 @@ class hawkes_process_classifier:
         for time_ctr in range(1, len(time_stamps)):
             r[time_ctr] = np.exp(-w * (time_stamps[time_ctr] - time_stamps[time_ctr - 1])) * (1 + r[time_ctr - 1])
 
-        first_sum = np.sum(1.0/(mu + alpha * r))
+        # first_sum = np.sum(1.0/(mu + alpha * r))
 
-        # first_sum = 0
-        # for elt in r:
-        #     first_sum=first_sum+(1.0/(mu+alpha*elt))
+        first_sum = 0
+        for elt in r:
+            first_sum=first_sum+(1.0/max(1e-10,(mu+alpha*elt)))
 
-        del_mu = -time_stamps[-1] + first_sum
+        del_mu = (-time_stamps[-1] + first_sum)*np.exp(param)
 
-        # print(del_mu)
+        print("Negative of gradient: ", -del_mu)
 
         return -del_mu
 
@@ -66,13 +79,13 @@ class hawkes_process_classifier:
         # res = check_grad(self.neg_log_likelihood, self.gradient_mu, [], args = triplet)
         #
         for i in range(100):
-            mu=i/10
+            mu = i/10
 
-            print("Mu: ", mu, "Grad: ", check_grad(self.neg_log_likelihood, self.gradient_mu, np.array([mu]), *triplet))
+            print("Mu: ", mu, "Err: ", check_grad(self.neg_log_likelihood, self.gradient_mu, np.array([mu]), *triplet))
 
     def estimate_params(self, time_stamps):
-        mu0 = 0.1
-        w = 0.1
+        mu0 = np.log(0.1)
+        w = 0.01
         alpha = 0.1
 
         initial_guess = mu0
@@ -85,17 +98,7 @@ class hawkes_process_classifier:
         return res
 
     def main(self):
-        # print(self.estimate_params(np.array([2.8914986, 8.09118015, 10.35220284, 15.72422933,
-        #                           20.41385768, 24.76074457, 58.6856862, 59.66601112,
-        #                           86.23050124, 95.78644578, 97.79439315, 109.11365526,
-        #                           115.38968376, 115.99595348, 138.26479343, 156.01784925,
-        #                           178.28689279, 181.86195694, 193.8846334, 199.726623,
-        #                           206.28067149, 213.24882906, 225.44679125, 238.64851829,
-        #                           246.26655032, 250.73418687, 255.75212692, 273.53467842])
-        #                       )
-        #       )
-
-        self.check_grad_mu(np.array([2.8914986, 8.09118015, 10.35220284, 15.72422933,
+        res = self.estimate_params(np.array([2.8914986, 8.09118015, 10.35220284, 15.72422933,
                                   20.41385768, 24.76074457, 58.6856862, 59.66601112,
                                   86.23050124, 95.78644578, 97.79439315, 109.11365526,
                                   115.38968376, 115.99595348, 138.26479343, 156.01784925,
@@ -103,6 +106,20 @@ class hawkes_process_classifier:
                                   206.28067149, 213.24882906, 225.44679125, 238.64851829,
                                   246.26655032, 250.73418687, 255.75212692, 273.53467842])
                               )
+
+
+        print(res)
+
+        print("Estimated mu: ", np.exp(res.x))
+
+        # self.check_grad_mu(np.array([2.8914986, 8.09118015, 10.35220284, 15.72422933,
+        #                           20.41385768, 24.76074457, 58.6856862, 59.66601112,
+        #                           86.23050124, 95.78644578, 97.79439315, 109.11365526,
+        #                           115.38968376, 115.99595348, 138.26479343, 156.01784925,
+        #                           178.28689279, 181.86195694, 193.8846334, 199.726623,
+        #                           206.28067149, 213.24882906, 225.44679125, 238.64851829,
+        #                           246.26655032, 250.73418687, 255.75212692, 273.53467842])
+        #                       )
 
     def fit(self):
         pass
